faf5267f7f300d576cd015a669fff7590967d662
JCaneso <cookiemonsterjcc96@gmail.com>
2020-07-06 04:52:13 -0700

    made port adaptable to heroku

--- server.js
+++ server.js
@@ -23,4 +23,7 @@ db.once('open', () => console.log("Connected to mongoose"))
 
 app.use('/', indexRouter)
 
-app.listen(process.env.POTY || 5000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
